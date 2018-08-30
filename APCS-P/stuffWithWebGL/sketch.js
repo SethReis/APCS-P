60c27e8daa401b2f9877f35cde7fcb46717eb654
SethReis <795091@seq.org>
2018-08-30 08:48:36 -0700

    more than 1 ball bounces weird

--- APCS-P/stuffWithWebGL/sketch.js
+++ APCS-P/stuffWithWebGL/sketch.js
@@ -1,65 +1,56 @@
-var angle = 0.7;
 var shapes = [];
-var loc;
-var vel;
 //  This is a comment
 //  The setup function function is called once when your program begins
 function setup() {
   var cnv = createCanvas(800, 800, WEBGL);
-  loc = createVector(0, 0);
-  vel = createVector(random(-3, 3), random(-3, 3));
-  // for (var i = 0; i < 1; i++){
-  //   shapes.push(new Shape());
-  // }
+  for (var i = 0; i < 2; i++){
+    let loc = createVector(0, 0);
+    let vel = createVector(random(-5, 5), random(-5, 5));
+    let acc = createVector(0, random(0.1, 0.9))
+    shapes.push(new Shape(loc, vel, acc));
+  }
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
-  var dx = mouseX - width/2;
-  var dy = mouseY - height/2;
-  var v = createVector(dx, dy, 0);
-  v.normalize();
+  // var dx = mouseX - width/2;
+  // var dy = mouseY - height/2;
+  // var v = createVector(dx, dy, 0);
+  // v.normalize();
+  //
+  pointLight(0, 0, 255, 0, 0, 250);
 
-  //ambientLight(0, 0, 255);
-  directionalLight(255, 255, 0, v);
+  // directionalLight(255, 255, 0, v);
 
   background(175);
-
-  rotateX(angle);
-  rotateY(angle * 0.1);
-  rotateZ(angle * 0.2);
-
-  loc.add(vel);
-  translate(loc.x, loc.y);
-  if(loc.x >= 400 || loc.x <= -409){
-    vel.x = -vel.x;
+  for (var i = 0; i < shapes.length; i++){
+    shapes[i].move();
+    shapes[i].display();
   }
-  if(loc.y >= 400 || loc.y <= -400){
-    vel.y = -vel.y;
-  }
-
-  noStroke();
-  specularMaterial(255);
-  sphere(100);
-  // for (var i = 0; i < shapes.length; i++){
-  //   shapes[i].turn();
-  //   shapes[i].display();
-  // }
-  angle += 0.01;
 }
 
-// function Shape() {
-//   torus(100, 20);
-//
-//   this.turn = function() {
-//     rotateX(angle);
-//     rotateY(angle * 0.1);
-//     rotateZ(angle * 0.2);
-//   }
-//
-//   this.display = function() {
-//     specularMaterial(255);
-//     noStroke();
-//     torus(100,20);
-//   }
-// }
+function Shape(loc, vel, acc) {
+  ambientMaterial(255);
+  sphere(50);
+  this.loc = loc;
+  this.vel = vel;
+  this.acc = acc;
+
+  this.move = function() {
+    this.loc.add(this.vel);
+    this.vel.add(this.acc);
+    translate(this.loc.x, this.loc.y);
+    if(this.loc.x >= 350 || this.loc.x <= -350){
+      this.vel.x = -this.vel.x;
+    }
+    if(this.loc.y >= 350 || this.loc.y <= -350){
+      this.vel.y = -(this.vel.y-this.acc.y);
+    }
+  }
+
+  this.display = function() {
+    noStroke();
+    ambientMaterial(255);
+    sphere(50);
+  }
+}
