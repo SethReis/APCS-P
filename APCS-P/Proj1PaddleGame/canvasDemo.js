0d1cd6fbad343363ca33bbd6b099f77bbafbf014
Seth Reis <795091@seq.org>
2018-10-09 08:51:24 -0700

    working on it

--- APCS-P/Proj1PaddleGame/canvasDemo.js
+++ APCS-P/Proj1PaddleGame/canvasDemo.js
@@ -17,8 +17,13 @@ function init(){
   // get the context
   ctx = canvas.getContext('2d'); // This is the context
   for(var i = 0; i < 2; i++){
-     balls.push(new Ball());
-     }
+    balls.push(new Ball());
+  }
+
+  canvas.addEventListener('mousemove', function(){
+    mouseX = event.offsetX;
+  }, false);
+
 
   animate(); // Call to your animate function
 }
@@ -30,7 +35,7 @@ function animate(){
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   for (var i = 0; i < balls.length; i++){
-    // add barrier to velocity //
+    //render and update a single ball
     var ball = balls[i];
     ctx.beginPath();
     ctx.arc(ball.x, ball.y, ball.rad, 0, Math.PI*2);
@@ -47,22 +52,38 @@ function animate(){
     ball.x += ball.dx
     ball.y += ball.dy;
     ball.dy += 0.5;
-      }
 
+    if((ball.y + ball.rad) - (paddle.y - paddle.h) >= 0
+    && (ball.y - ball.rad) - (paddle.y + paddle.h) >= 0
+    && (ball.x + ball.rad) - (paddle.x - paddle.w) >= 0
+    && (ball.x - ball.rad) - (paddle.x + paddle.w) >= 0){
+      balls.splice(i, 1);
+    }
+  }
+  ctx.beginPath();
+  ctx.rect(paddle.x-paddle.w, paddle.y-paddle.h, paddle.w*2, paddle.h*2);
+  ctx.fillStyle = paddle.c;
+  ctx.fill();
+  ctx.strokeStyle = paddle.c;
+  ctx.stroke();
+  paddle.x = mouseX;
 }
 
 function Ball(){
   this.x = Math.random()*innerWidth + 100;
   // this.y = Math.random()*innerHeight + 100;
-  //this.x = 1000;
+  // this.x = 1000;
   this.y = 100;
   this.dx = Math.random()*10-5;
   this.dy = Math.random()*10-5;
   this.rad = Math.random()*10+10;
-  this.c = 'rgba(' + Math.floor(Math.random()*225) + ',' + Math.floor(Math.random()*225) + ',' + Math.floor(Math.random()*225) + ',' + Math.random() + ')';
+  this.c = 'rgba(' + Math.floor(Math.random()*225) + ',' + Math.floor(Math.random()*225) + ',' + Math.floor(Math.random()*225) + ',' + 1 + ')';
 }
 
 function Paddle(){
-  this.x = 1000;
-  this.y = 800;
+  this.x = innerWidth/2;
+  this.y = innerHeight/2 + innerHeight/4;
+  this.w = 200;
+  this.h = 50;
+  this.c = 'rgba(' + 255 + ',' + 255 + ',' + 255 + ',' + 1 + ')';
 }
